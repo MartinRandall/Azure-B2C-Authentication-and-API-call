5f4f525e1d4b8d645849a171b47c8fc580ce806b
Martin Randall <martin@coryl.com>
2022-09-25 21:26:19 +0100

    Updated the GetCompaniesButton.tsx to useEffect to get pre-fetch the access token.

--- src/components/GetCompaniesButton.tsx
+++ src/components/GetCompaniesButton.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {useMsal} from '@azure/msal-react';
 import {scopes} from '../auth/authConfig';
 import {PagedResponse} from '../models/pagedResponse';
@@ -7,31 +7,25 @@ import {Company} from '../models/company';
 export type GetCompaniesButtonProps = {}
 
 export default function GetCompaniesButton({}: GetCompaniesButtonProps) {
-    const { instance, accounts} = useMsal();
+    const {instance, accounts} = useMsal();
     const [accessToken, setAccessToken] = useState<string>('');
     const [companies, setCompanies] = useState<Array<Company>>([]);
 
-    const RequestAccessToken = async () => {
+    /// Get an access token for the API when the component is mounted
+    useEffect(() => {
         const request = {
             ...scopes,
             account: accounts[0]
         };
 
-        try {
-            const response = await instance.acquireTokenSilent(request);
-            setAccessToken(response.accessToken);
-            console.log(response.accessToken);
-            return response.accessToken;
-        } catch (error) {
-            const response = await instance.acquireTokenPopup(request);
-            setAccessToken(response.accessToken);
-            return response.accessToken;
-        }
-    };
-
+        instance.acquireTokenSilent(request)
+            .then(response => setAccessToken(response.accessToken))
+            .catch(e => {
+                instance.acquireTokenPopup(request).then(response => setAccessToken(response.accessToken))
+            });
+    }, []);
 
     const getCompanies: () => Promise<PagedResponse<Company> | null> = async () => {
-        const accessToken = await RequestAccessToken();
         const headers = new Headers();
         const bearer = `Bearer ${accessToken}`;
         headers.append("Authorization", bearer);
@@ -54,7 +48,7 @@ export default function GetCompaniesButton({}: GetCompaniesButtonProps) {
 
     return (
         <>
-        <button onClick={onCompanyButtonClicked}>Get Companies</button>
+            <button onClick={onCompanyButtonClicked}>Get Companies</button>
 
             <ul>
                 {companies.map(company => <li key={company.id}>{company.name}</li>)}
